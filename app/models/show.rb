e3b30c7cdfceae236c54b66c9551d5b3a9923490
Kyle <khartson2017@gmail.com>
2022-06-11 20:11:26 -0400

    completed assignment

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        actors_list = []
+        self.actors.each do |a|
+            actors_list << a.full_name
+        end
+        actors_list
+    end 
 end
